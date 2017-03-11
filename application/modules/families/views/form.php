23e2468ddf2d9f2470d25188a122d5a94b3b00a6
whitebird128 <dan_smit@zoho.com>
2017-03-12 00:49:21 +0800

    rearrange code

--- application/modules/families/views/form.php
+++ application/modules/families/views/form.php
@@ -25,7 +25,7 @@
             </div>
             <div class="col-xs-12 col-sm-6">
                 <input type="text" name="family_name" id="family_name" class="form-control"
-                       value="<?php echo $this->mdl_families->form_value('family_name'); ?>">
+                       value="<?php echo $this->mdl_families->form_value('family_name'); ?>" />
             </div>
         </div>
 
